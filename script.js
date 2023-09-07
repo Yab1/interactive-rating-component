407cfb9755e8cb26d042ff7dedb6741e6a3040f3
Yab1 <yabilisanu@gmail.com>
2023-09-08 01:28:15 +0300

    Frontend Mentor Challenge

---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("#form");
+
+const feedback = document.querySelector("#feedback");
+
+const button = document.querySelector("button");
+button.addEventListener("click", handleSubmit);
+
+const rateWrapper = document.querySelector(".rate-wrapper");
+const rates = rateWrapper.querySelectorAll("span");
+rates.forEach((rate) => {
+  rate.addEventListener("click", (e) => handleSelected(e));
+});
+
+const selectedText = document.querySelector(".status");
+const statusSpan = selectedText.querySelector("span");
+
+let count = 0;
+
+function handleSelected(e) {
+  count++;
+  e.target.classList.add("active");
+}
+
+function handleSubmit() {
+  form.classList.add("hidden");
+  form.classList.remove("visible");
+  feedback.classList.add("visible");
+  feedback.classList.remove("hidden");
+  statusSpan.innerHTML = count;
+}
